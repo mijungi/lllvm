f4d198797829afaed4d17dae56e18e2a005be651
mijung <brients@gmail.com>
2015-10-06 21:01:48 +0100

    eq 28 and 29 are correct

--- script/sanity_check.m
+++ script/sanity_check.m
@@ -33,28 +33,31 @@ L = diag(h) - G;
 
 %% (2) generate x and C
 
-% invOmega = kron(2*L,speye(dx));
-% x = mnormrnd(zeros(dx, n), 1, inv(alpha*eye(n) + 2*L) );
-% X = x(:);
-% 
-% U = eye(dy);
-% ep_invOmega = epsilon*eye(size(invOmega)) + invOmega;
-% v = mnormrnd(zeros(dy, dx*n), U, inv(ep_invOmega) );
-% C = v(:);
-% 
-% %% (3) generate y
-% 
-% invV = gamma*speye(dy);
-% V_y = inv(epsilon*eye(n) + 2*L*gamma);
-% E = generateE(G, reshape(C,dy,n*dx), invV, X(:));
-% E = reshape(E, dy, n);
-% mu_y = E*V_y;
-% Y = mnormrnd(mu_y, 1, V_y);
-% y = Y(:);
+invOmega = kron(2*L,speye(dx));
+x = mnormrnd(zeros(dx, n), 1, inv(alpha*eye(n) + 2*L) );
+X = x(:);
+
+U = eye(dy);
+J = kron(ones(n,1), eye(dx));
+ep_invOmega = epsilon*J*J' + invOmega;
+v = mnormrnd(zeros(dy, dx*n), U, inv(ep_invOmega) );
+C = v(:);
+
+%% (3) generate y
+
+invV = gamma*speye(dy);
+ones_n = ones(n,1);
+
+V_y = inv(epsilon*ones_n*ones_n' + 2*L*gamma);
+E = generateE(G, reshape(C,dy,n*dx), invV, X(:));
+E = reshape(E, dy, n);
+mu_y = E*V_y;
+Y = mnormrnd(mu_y, 1, V_y);
+y = Y(:);
 
 %% check prior on x
 
-x = randn(dx, n);
+% x = randn(dx, n);
 
 One_vec_n = ones(n,1);
 invOmega = kron(2*L,speye(dx));
@@ -82,17 +85,18 @@ log_prob_x_eq = exponent_eq -0.5*n*dx*log(2*pi) + 0.5*logdetns(invPi);
 
 %% check prior on C (we don't need beta, so remove it from the model)
 
-v = randn(dy, dx*n);
+% v = randn(dy, dx*n);
 
-J = kron(ones(n,1), eye(dx));
+% J = kron(ones(n,1), eye(dx));
 
-log_prob_c = log(mvnpdf(v(:)', zeros(1, n*dx*dy), kron(inv(epsilon*J*J' + invOmega), eye(dy))));
+before_log = mvnpdf(v(:)', zeros(1, n*dx*dy), kron(inv(epsilon*J*J' + invOmega), eye(dy))); 
+log_prob_c = log(before_log);
 exponent = -epsilon*0.5*trace(v*J*J'*v') - 0.5*trace(invOmega*v'*v);
 
 dist = zeros(n, n);
 for i=1:n
     for j=1:n
-        [i j]
+%         [i j]
         eta = G(i,j);
         if eta~=0
             dist(i,j) = 0.5*sum(sum((v(:,1+(i-1)*dx:i*dx) - v(:,1+(j-1)*dx:j*dx)).^2));
@@ -108,11 +112,11 @@ log_prob_c_eq = exponent  - 0.5*n*dx*dy*log(2*pi) + 0.5*dy*logdetns(epsilon*J*J'
 
 %% check likelihood
 
-y = randn(dy, n);
+% Y = randn(dy, n);
 
-ones_n = ones(n,1);
+% ones_n = ones(n,1);
 
-% [-0.5*epsilon*sum(sum(y,2).^2)  -0.5*y(:)'*kron(epsilon*ones_n*ones_n', eye(dy))*y(:)]
+% [-0.5*epsilon*sum(sum(Y,2).^2)  -0.5*Y(:)'*kron(epsilon*ones_n*ones_n', eye(dy))*Y(:)]
 
 invV = gamma*eye(dy);
 Cmat = reshape(v, dy, dx, n);
@@ -120,15 +124,15 @@ E = compute_E(G, invV, Cmat, x, dx, dy, n);
 E = reshape(E, dy, n);
 
 M_y = E/(epsilon*ones_n*ones_n' + 2*gamma*L); 
-exponent = - 0.5*trace(inv(epsilon*ones_n*ones_n' + 2*gamma*L)*(y-M_y)'*(y-M_y)); 
+exponent = - 0.5*trace(inv(epsilon*ones_n*ones_n' + 2*gamma*L)*(Y-M_y)'*(Y-M_y)); 
 
 dist = zeros(n, n);
 for i=1:n
     for j=1:n
-        [i j]
+%         [i j]
         eta = G(i,j);
         if eta~=0
-            lin_dist = y(:,j) - y(:,i) - v(:, 1+(i-1)*dx:i*dx)*(x(:,j) - x(:,i));
+            lin_dist = Y(:,j) - Y(:,i) - v(:, 1+(i-1)*dx:i*dx)*(x(:,j) - x(:,i));
             dist(i,j) = 0.5*gamma*lin_dist'*lin_dist;
         end
     end
@@ -150,15 +154,37 @@ mu_y = sig_y*E(:);
 
 % [y(:)'*E(:) (mu_y'/sig_y)*y(:)]
 % [f (mu_y'/sig_y)*mu_y]
-exponent_eq_14 = - sum(sum(dist))-0.5*epsilon*sum(sum(y,2).^2);
-exponent_eq_15 =  -0.5*((y(:)'/sig_y)*y(:)-2*y(:)'*E(:)+f);
+exponent_eq_14 = - sum(sum(dist))-0.5*epsilon*sum(sum(Y,2).^2);
+exponent_eq_15 =  -0.5*((Y(:)'/sig_y)*Y(:)-2*Y(:)'*E(:)+f);
 [exponent_eq_14 exponent_eq_15]
 
-log_prob_y = log(mvnpdf(y(:)', mu_y', sig_y));
+log_prob_y = log(mvnpdf(Y(:)', mu_y', sig_y)); % eq. (22) 
 
 logZy = 0.5*((mu_y'/sig_y)*mu_y - f) + 0.5*logdetns(2*pi*sig_y);
 log_prob_y_eq = exponent_eq_15 -logZy;
-[log_prob_y log_prob_y_eq]
+[log_prob_y log_prob_y_eq] % eq. (22) is the same as eq(27). 
+
+%% check if eq(27) and eq(28) are the same
+% y'e = x'b
+% and y'e = trace(C'inv(V)H)
+
+ye = y'*E(:); 
+display(sprintf('y trp e: %.3f',ye ));
+
+diagU = 1; 
+cov_c = 1e-6*eye(n*dx);
+ [~, b] = compute_invA_qC_and_B_qC(G, v, cov_c, invV, y, n, dy, dx, diagU); 
+xb = x(:)'*b; 
+display(sprintf('x trp b: %.3f',xb ));
+
+cov_x = 1e-6*eye(n*dx);
+ [~, H] = compute_invGamma_qX_and_H_qX(G, x(:), cov_x, y, n, dy, dx);
+trCVinvH = trace(v'*invV*H); 
+display(sprintf('tra(CtrpinvVH): %.3f', trCVinvH ));
+
+
+
+
 
 
 
