ebdd28d9bfcbd513e97e9d593ae3566c3ebafd6d
MijungTheGatsbyPostdoc <mijung@gatsby.ucl.ac.uk>
2015-10-27 22:16:47 +0000

    fixing alpha = 1e-3 for USPS data and simulated data (sorry, Wittawat, I am going to leave this record so that I don't forget

--- lllvm/negDkl_x_update_alpha.m
+++ lllvm/negDkl_x_update_alpha.m
@@ -12,11 +12,13 @@ mean_x2 = mean_x(:)'*mean_x(:);
 tr_cov_x = trace(cov_x);
 
 % alpha update
-% eigv_L = eigv_L(1:end-1); % picking up nonzero eigenvalues
-min_obj = @(a) -( dx*sum(log(a + 2*eigv_L)) - a*tr_cov_x - a*mean_x2 );
-opt = struct();
-opt.TolX = 1e-4;
-[alpha] = fminbnd(min_obj, 1e-5, 500, opt);
+% min_obj = @(a) -( dx*sum(log(a + 2*eigv_L)) - a*tr_cov_x - a*mean_x2 );
+% opt = struct();
+% opt.TolX = 1e-4;
+% [alpha] = fminbnd(min_obj, 1e-5, 500, opt);
+
+% alpha = eigv_L(end-1);
+alpha = 1e-3; 
 
 % compute the lower bound with the updated alpha 
 % lwb_x  = 0.5*logdetns(cov_x) + 0.5*logdetns(alpha*eye(ndx) + invOmega)  ...
