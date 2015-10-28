16c3862ab243bcecc91297746227c14181d54449
MijungTheGatsbyPostdoc <mijung@gatsby.ucl.ac.uk>
2015-10-28 21:39:18 +0000

    save G as well

--- script/testscript_artificial_data.m
+++ script/testscript_artificial_data.m
@@ -6,23 +6,28 @@ clc;
 
 % n = 400; % total number of datapoints
 % data_flag = 1; % 3D Gaussian
-% kmat = 4:2:18; 
+% kmat = 6:2:18; 
+
 % kmat = [5, 10, 20, 40, 80, 160];
 
 data_flag = 3; % swiss roll
 n = 400; % total number of datapoints
-kmat = 4:11;
+% kmat = 6:11;
+kmat = 9; 
 
 % maximum number of EM iterations to perform
 max_em_iter = 50;
 
-maxseed = 1;
+maxseed = 10;
 
-for seednum = 1:maxseed
+for seednum = 4
+% for seednum = maxseed:-1:1
     
     oldRng = rng();
     rng(seednum);
-    display(sprintf('seednum %d out of %d', seednum, maxseed));
+    display(sprintf('seednum %d', seednum));
+    
+%     display(sprintf('seednum %d out of %d', seednum, maxseed));
     
     for kidx = 1:length(kmat)
         
@@ -54,7 +59,7 @@ for seednum = 1:maxseed
         op.max_em_iter = max_em_iter;
         % absolute tolerance of the increase of the likelihood.
         % If (like_i - like_{i-1} )  < abs_tol, stop EM.
-        op.abs_tol = 1e-1;
+        op.abs_tol = 1e-3;
         op.G = G;
         op.dx = dx;
         % The factor to be added to the prior covariance of C and likelihood. This must be positive and typically small.
@@ -64,7 +69,7 @@ for seednum = 1:maxseed
         op.alpha0 = rand;
         % initial value of gamma.  V^-1 = gamma*I_dy where V is the covariance in the
         % likelihood of  the observations Y.
-        op.gamma0 = 0.1*rand;
+        op.gamma0 = rand;
         %recorder = create_recorder('print_struct');
         store_every_iter = 5;
         only_cov_diag = false;
@@ -78,7 +83,7 @@ for seednum = 1:maxseed
         
         [results, op ] = lllvm_1ep(Y, op);
         
-        save(strcat('fixingalpha_dataflag ', num2str(data_flag), 'seednum ', num2str(seednum), 'k ', num2str(k), '.mat'), 'results', 'Yraw', 'col');
+        save(strcat('fixingalpha_dataflag ', num2str(data_flag), 'seednum ', num2str(seednum), 'k ', num2str(k), '.mat'), 'results', 'Yraw', 'col', 'truex');
 
 %         save(strcat('dataflag ', num2str(data_flag), 'seednum ', num2str(seednum), 'k ', num2str(k), '.mat'), 'results');
         
@@ -130,14 +135,14 @@ end
 % 
 % Yraw = Y;
 %     %%    
-% which = 3;
+% which = 50;
 % 
-% figure(1);
+% figure(3);
 % plotlearning(dx,dy,n,reshape(results.mean_c(:,which),dy,n*dx),Yraw);
 % 
-% figure(2);
+% figure(4);
 % subplot(211); plot(results.lwbs);
 % reshaped_mean_x = reshape(results.mean_x(:,which), dx, []);
 % subplot(212); scatter(reshaped_mean_x(1,:), reshaped_mean_x(2,:), 20, col, 'o', 'filled');
-% 
+
 % rng(oldRng);
