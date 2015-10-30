1356283dea6bdb4d8008ae0ef99bdbc200ab83c7
Wittawat Jitkrittum <wittawatj@gmail.com>
2015-10-30 13:56:53 +0000

    update startup.m to use data/ instead of real_data/

--- code/startup.m
+++ code/startup.m
@@ -7,7 +7,7 @@ fs = filesep();
 addpath(pwd);
 
 % folders to be added by genpath
-gfolders = {'helper', 'lllvm', 'script', 'thirdparty', 'real_data/ushcn_v2.5/', ...
+gfolders = {'helper', 'lllvm', 'script', 'thirdparty', 'data/ushcn_v2.5/', ...
     'ex'};
 for i=1:length(gfolders)
     fol = gfolders{i};
@@ -16,7 +16,7 @@ for i=1:length(gfolders)
     addpath(genpath(p));
 end
 
-addpath('real_data');
+%addpath('real_data');
 
 clear base fs gfolders i fol p 
 
